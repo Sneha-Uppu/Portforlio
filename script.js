1560e66c3a781279a056222b19b5c43a1827e918
Sneha-Uppu <81636785+Sneha-Uppu@users.noreply.github.com>
2022-06-18 18:38:07 +0530

    second commit

--- script.js
+++ script.js
@@ -1,55 +1,69 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function()
+ {
+    $(window).scroll(function()
+      {
         // sticky navbar on scroll script
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
-        }else{
+        }
+        else
+        {
             $('.navbar').removeClass("sticky");
         }
         
         // scroll-up button show/hide script
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
-        }else{
+        }
+        else
+        {
             $('.scroll-up-btn').removeClass("show");
         }
     });
+    
     // slide-up script
-    $('.scroll-up-btn').click(function(){
+    $('.scroll-up-btn').click(function()
+     {
         $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
+    $('.navbar .menu li a').click(function()
+    {
         $('html').css("scrollBehavior", "smooth");
     });
-    // toggle menu/navbar script
+
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+    
     // typing text animation script
-    var typed = new Typed(".typing", {
+    var typed = new Typed(".typing", 
+       {
         strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
-    var typed = new Typed(".typing-2", {
+    
+    var typed = new Typed(".typing-2", 
+     {
         strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
+    
     // owl carousel script
-    $('.carousel').owlCarousel({
+    $('.carousel').owlCarousel(
+        {
         margin: 20,
         loop: true,
         autoplay: true,
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
-        responsive: {
+        responsive: 
+            {
             0:{
                 items: 1,
                 nav: false
